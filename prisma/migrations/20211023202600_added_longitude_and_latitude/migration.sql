d22e6abe152456485880c7278b329ebec8d77967
Lizzie Hand <elizabethghand@gmail.com>
2021-10-23 21:28:10 +0100

    Added longitude and latitude data

---
+++ prisma/migrations/20211023202600_added_longitude_and_latitude/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Restaurant" ALTER COLUMN "latitude" DROP NOT NULL,
+ALTER COLUMN "longitude" DROP NOT NULL;
--- utils/mockdata.js
+++ utils/mockdata.js
@@ -4,111 +4,155 @@ const restaurants = [
     {
         name: "Curry King",
         address: "326 Old St, London EC1V 9DR",
+        longitude: -0.089770,
+        latitude: 51.525960,
         image: faker.image.food()
     },
     {
         name: "Attendant",
         address: "74 Great Eastern St, London EC2A 3JL",
+        longitude: -0.082520,
+        latitude: 51.525720,
         image: faker.image.food()
     },
     {
         name: "Old Street Cafe",
         address: "36 Old St, London EC1V 9AE",
+        longitude: -0.097670,
+        latitude: 51.523540,
         image: faker.image.food()
     },
     {
         name: "Grind",
         address: "213 Old St, London EC1V 9NR",
+        longitude: -0.088640,
+        latitude: 51.525532,
         image: faker.image.food()
     },
     {
         name: "Look mum no hands!",
         address: "49 Old St, London EC1V 9HX",
+        longitude: -0.088070,
+        latitude: 51.525509,
         image: faker.image.food()
     },
     {
         name: "Origin Coffee",
         address: " 65 Charlotte Rd, London EC2A 3PE",
+        longitude: -0.081190,
+        latitude: 51.526100,
         image: faker.image.food()
     },
     {
         name: "Cafe Arena.",
         address: "127 City Rd, London EC1V 1JB",
+        longitude: -0.087690,
+        latitude: 51.526450,
         image: faker.image.food()
     },
     {
         name: "Ozone Coffee Roasters, Shoreditch",
         address: "11 Leonard St, London EC2A 4AQ",
+        longitude: -0.086000,
+        latitude: 51.524690,
         image: faker.image.food()
     },
     {
         name: "Clear Hearts",
         address: "326 Old St, London EC1V 9DR",
+        longitude: -0.089770,
+        latitude: 51.525959,
         image: faker.image.food()
     },
     {
         name: "Hermanos Colombian Coffee Roasters",
         address: "Station Market Stall, Old St, London EC1Y 1BE",
+        longitude: -0.090750,
+        latitude: 51.525280,
         image: faker.image.food()
     },
     {
         name: "Lennies Sandwich Bar",
         address: "Imperial Hall, 3-5 Old Street Station, London EC1Y 1BE",
+        longitude: -2.125800,
+        latitude: 53.265190,
         image: faker.image.food()
     },
     {
         name: "Fix 126",
         address: "Imperial Hall, 3-5 Old Street Station, London EC1Y 1BE",
+        longitude: -2.125800,
+        latitude: 53.265190,
         image: faker.image.food()
     },
     {
         name: "Lennies Sandwich Bar",
         address: "126 Curtain Rd, London EC2A 3PJ",
+        longitude: -0.080240,
+        latitude: 51.526170,
         image: faker.image.food()
     },
     {
         name: "The Fallow Deer Cafe",
         address: "130 High St, Teddington TW11 8JB",
+        longitude: -0.328000,
+        latitude: 51.427370,
         image: faker.image.food()
     },
     {
         name: "Jacks Sandwich Bar",
         address: "Imperial Hall, 3-5 Old Street Station, London EC1Y 1BE",
+        longitude: -2.125800,
+        latitude: 53.265190,
         image: faker.image.food()
     },
     {
         name: "Sidra - Teddington",
         address: "5 Church Rd, Teddington TW11 8PF",
+        longitude: -0.336420,
+        latitude: 51.426590,
         image: faker.image.food()
     },
     {
         name: "Lennies Sandwich Bar",
         address: "Imperial Hall, 6-8 Old Street Station, London EC1Y 1BE",
+        longitude: -72.079100,
+        latitude: 41.528230,
         image: faker.image.food()
     },
     {
         name: "Gosia's Cafe and Deli",
         address: "52 Broad St, Teddington TW11 8QY",
+        longitude: -0.337030,
+        latitude: 51.425900,
         image: faker.image.food()
     },
     {
         name: "Flying Cloud Cafe",
         address: "27 Ferry Rd, Teddington TW11 9NN",
+        longitude: -0.325180,
+        latitude: 51.428110,
         image: faker.image.food()
     },
     {
         name: "Milos Cafe",
         address: "16 High St, Teddington TW11 8EW",
+        longitude: -0.325180,
+        latitude: 51.428110,
         image: faker.image.food()
     },
     {
         name: "The French Tarte",
         address: "45 High St, Teddington TW11 8HA",
+        longitude: -0.330210,
+        latitude: 51.427060,
         image: faker.image.food()
     },
     {
         name: "The Coffee Mill",
         address: "91 High St, Teddington TW11 8HG",
+        longitude: -0.329650,
+        latitude: 51.427300,
         image: faker.image.food()
     },
 
