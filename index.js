5a7f5f74ce9ec74dac8cc621547a006ab177878d
Lizzie Hand <elizabethghand@gmail.com>
2021-10-23 19:20:30 +0100

    Implement cors

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ const app = express()
 
 app.disable("x-powered-by")
 
-app.use(cors())
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
 
 /* SETUP ROUTES */
 
+app.use("/user", usersRouter)
 app.use("/categories", categoryRouter);
 app.use("/deals", dealRouter);
 app.use("/cafe", cafeRouter);
-app.use("/register", usersRouter)
 
 app.get("*", (req, res) => {
     res.json({ ok: true })
