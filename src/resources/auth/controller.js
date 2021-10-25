69604a06707a6e2e20e6ddb2d8adb29449ee2257
Lizzie Hand <elizabethghand@gmail.com>
2021-10-25 15:39:58 +0100

    Create token

--- src/resources/auth/controller.js
+++ src/resources/auth/controller.js
@@ -1,10 +1,19 @@
 const { findUserWithValidation } = require("./service");
+const { createToken } = require("../../../utils/authgenerator")
 
 const loginUser = async (req, res) => {
     const userCreds = req.body
 
     try {
         const loggedUser = await findUserWithValidation(userCreds)
+
+
+        const token = createToken({
+            id: loggedUser.id,
+            role: loggedUser.role,
+        });
+
+        res.cookie("token", token, { httpOnly: true });
         res.json(loggedUser)
 
     } catch (error) {
--- utils/authgenerator.js
+++ utils/authgenerator.js
@@ -1,8 +1,10 @@
-const { jwt } = require('jsonwebtoken');
-const { dotenv } = require("dotenv");
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
 
 const JWT_SECRET = process.env.JWT
 
 const createToken = async (payload) => {
     return jwt.sign(payload, JWT_SECRET)
 }
+
+module.exports = { createToken }
